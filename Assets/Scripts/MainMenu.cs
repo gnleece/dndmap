00923bae3a710d1905c606a9eaca005519a895c7
Gwynneth Leece <gleece@gmail.com>
2019-07-07 11:29:02 -0700

    Add StandaloneFileBrowser package for file browsing for image import

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,11 +1,28 @@
-﻿using System.Collections;
+﻿using SFB;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField]
+    private SpriteRenderer output;
+
     public void Button_LoadImage()
     {
         Debug.Log("Load image");
+        var paths = StandaloneFileBrowser.OpenFilePanel("Title", "", "jpg", false);
+        if (paths.Length > 0)
+        {
+            StartCoroutine(OutputRoutine(new System.Uri(paths[0]).AbsoluteUri));
+        }
+    }
+
+    private IEnumerator OutputRoutine(string url)
+    {
+        var loader = new WWW(url);
+        yield return loader;
+        output.sprite = Sprite.Create(loader.texture, new Rect(0,0, loader.texture.width, loader.texture.height), new Vector2(0.5f, 0.5f));
     }
 }
