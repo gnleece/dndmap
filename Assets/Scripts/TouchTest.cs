fa391dbc0ed92d05a3505624229e4788c7a3f01e
Gwynneth Leece <gleece@gmail.com>
2019-07-06 23:26:36 -0700

    Initial desgin doc; Touch input testing

---
+++ Assets/Scripts/TouchTest.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchTest : MonoBehaviour
+{
+    void Update()
+    {
+        Touch[] myTouches = Input.touches;
+        if (Input.touchCount > 0)
+        {
+            Debug.Log("TOUCHES:");
+            for (int i = 0; i < Input.touchCount; i++)
+            {
+                Debug.Log($"{myTouches[i].fingerId}");
+            }
+        }
+    }
+}
